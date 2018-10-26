270ca9d824f93616b2b11b75d7a6ec4a9cb023b0
GBTC406005UR <GBTC406005UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-26 13:50:33 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    @RequestMapping("/")
+    public String listMessages(Model model){
+        model.addAttribute("messages", messageRepository.findAll());
+        return "list";
+    }
+    @GetMapping("/add")
+    public String messageForm(Model model){
+        model.addAttribute("message", new Message());
+        return "messageform";
+    }
+    @PostMapping("/process")
+    public String processForm(@Valid @ModelAttribute("message") Message message,
+                              BindingResult result) {
+        if (result.hasErrors()) {
+            return "messageform";
+        }
+        messageRepository.save(message);
+        return "redirect:/";
+    }
+    @RequestMapping("/detail/{id}")
+    public String showMessage(@PathVariable("id") long id, Model model)
+    {
+        model.addAttribute("message", messageRepository.findById(id).get());
+        return "show";
+    }
+    @RequestMapping("/update/{id}")
+    public String updateMessage(@PathVariable("id") long id, Model model) {
+        model.addAttribute("message", messageRepository.findById(id).get());
+        return "messageform";
+    }
+    @RequestMapping("/delete/{id}")
+    public String delMessage(@PathVariable("id") long id){
+        messageRepository.deleteById(id);
+        return "redirect:/";
+
+    }
+}
